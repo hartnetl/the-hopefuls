6810f5f6ba0face7c010717c7d82b21931520445
Sharon-B <sharonbagnall@gmail.com>
2021-10-14 22:03:52 +0000

    Create story1.js file with initial variables

---
+++ assets/js/story1.js
@@ -0,0 +1,4 @@
+// Set global variables
+let storyTextEl = document.getElementById('story-text');
+let buttonOptionsEl = document.getElementById('button-options');
+let bgImage = document.getElementById('bg-image');
