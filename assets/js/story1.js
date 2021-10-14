7f22286ee04a08abae63c1b2b1d3483bbdc32749
Sharon-B <sharonbagnall@gmail.com>
2021-10-14 22:05:14 +0000

    Create startStory function

--- assets/js/story1.js
+++ assets/js/story1.js
@@ -2,3 +2,9 @@
 let storyTextEl = document.getElementById('story-text');
 let buttonOptionsEl = document.getElementById('button-options');
 let bgImage = document.getElementById('bg-image');
+
+
+// Create startStory function
+function startStory() {
+    showScene(1)
+}
