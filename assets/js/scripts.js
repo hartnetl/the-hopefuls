03f4710463d2bdf57fa059803a4acaf479650e63
MerveKucukzoroglu <mkucukzoroglu@gmail.com>
2021-10-15 09:06:35 +0000

    Add adio for adult/children buttons

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,43 +1,16 @@
 
-class AudioController {
-    constructor() {
-        this.btnClickAdult = new Audio('assets/audio/scream.wav');
-        this.btnClickChild = new Audio('assets/audio/evil_laugh.wav');
-        this.bgMusic = new Audio('assets/audio/bgAudio.mp3');
+let adultStoryBtn = new Audio('../assets/audio/scream.wav');
+let childtStoryBtn = new Audio('../assets/audio/evil_laugh.wav');
 
-        this.bgMusic.volume = 0.5;
-        this.bgMusic.loop = true;
-    }
+document.getElementById('adult-story').addEventListener('click', adultBtnAudio);
+document.getElementById('children-story').addEventListener('click', childBtnAudio);
 
-    startMusic(){
-        this.bgMusic.play();
-    }
-
-    stopMusic() {
-        this.bgMusic.pause();
-        this.bgMusic.currentTime = 0;
-    }
-
-    adultAudio() {
-        this.btnClickAdult.play();
-    }
-
-    childAudio(){
-        this.btnClickChild.play();
-    }
+function adultBtnAudio() {
+    adultStoryBtn.play();
+    adultStoryBtn.volume = 0.5; 
 }
 
-class buttonEvent {
-    constructor() {
-        this.audioController = new AudioController();
-    }
-    
-    adultAudio() {
-        this.AudioController.adultAudio();
-        document.getElementById('adult-story').addEventListener('click', play());
-    }
-
-    childAudio(){
-        this.btnClickChild.play();
-    }
+function childBtnAudio() {
+    childtStoryBtn.play();
+    childtStoryBtn.volume = 0.5; 
 }
